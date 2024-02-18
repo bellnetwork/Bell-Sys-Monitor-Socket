a2b3ed6dc670614b7a10db299331ff60d659a25d
Jefferson Jerome James <78507611+belljmks@users.noreply.github.com>
2024-02-18 00:01:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,10 @@
 from utils.sys.config.flask_config import create_app
 from utils.imports.emit.import_routes import setup_all_socketio_events
 
-# Initialize Flask and Socket.IO
-app, socketio, emit, redis_client, mail, celery, server_ip, server_id = create_app()
+app, socketio, emit = create_app()
 
-# Register all Socket.IO event handlers
+# Call the function to register all event handlers
 setup_all_socketio_events(socketio, emit)
-
-# Continue with the rest of your app setup...
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=9374)
